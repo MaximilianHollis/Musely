7fbbccabe711bf42259c6e5ea3a9cc7f52fd009e
Maximilian Hollis <hollismaximilian@gmail.com>
2022-07-12 23:34:59 -0400

    work on slider

--- src/components/overlay/index.tsx
+++ src/components/overlay/index.tsx
@@ -1,7 +1,50 @@
+import { motion } from 'framer-motion'
+import Slider from './slider'
 import styles from './index.module.css'
 
 export default () => (
-	<div className={styles.overlay}>
-		<div className={styles.overlay_content} />
-	</div>
+	<motion.div
+		className={styles.overlay}
+		initial={{ y: 120 }}
+		animate={{
+			y: 0,
+		}}
+		transition={{
+			type: 'tween',
+			duration: 0.2,
+		}}
+	>
+		<div className={styles.overlay_content}>
+			<div className={styles.information}>
+				<img
+					draggable={false}
+					src="https://images.unsplash.com/photo-1655047273143-91261102716f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+					alt="album"
+					className={styles.image}
+				/>
+				<div
+					style={{
+						alignItems: 'flex-start',
+						display: 'flex',
+						flexDirection: 'column',
+						padding: '0 4px',
+					}}
+				>
+					<p className={styles.title}>Titlefassssssssssssssssssssssss</p>
+					<p className={styles.artist}>Artist</p>
+				</div>
+			</div>
+			<div className={styles.controls}>
+				<div className={styles.buttons}>
+					back
+					<button type="button" className={styles.play}>
+						a
+					</button>
+					forw
+				</div>
+				<Slider />
+			</div>
+			<div className={styles.settings}>settings</div>
+		</div>
+	</motion.div>
 )
---
+++ src/components/overlay/slider.tsx
@@ -0,0 +1,9 @@
+import styles from './slider.module.css'
+
+export default () => (
+	<div className={styles.container}>
+		<div className={styles.bar}>
+			<div className={styles.circle} />
+		</div>
+	</div>
+)
